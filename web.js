38576ff6e55d35d58795bf321b85cedb302df1a9
Laura Hamilton <lauradhamilton@gmail.com>
2013-07-05 19:18:29 +0200

    Let's serve the index.html file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var
+express = require('express'),
+fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buf = new Buffer(fs.readFileSync(__dirname + '/index.html'));
+    var indexFile = buf.toString();
+    response.send(indexFile);
 });
 
 var port = process.env.PORT || 5000;
