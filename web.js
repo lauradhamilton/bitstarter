b50a2bb860dcfda20cf8812c7a37d009c4e1754d
Laura Hamilton <lauradhamilton@gmail.com>
2013-08-04 20:08:47 +0300

    Upgrading to bootstrap 3 and using the carousel template

--- web.js
+++ web.js
@@ -11,6 +11,7 @@ app.get('/', function(request, response) {
 });
 
 app.use('/public', express.static(__dirname + "/public"));
+app.use('/assets', express.static(__dirname + "/assets"));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
